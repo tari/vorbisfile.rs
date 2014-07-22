64f21e47b3bbdb37626afcc06160bb50d4cadf73
Peter Marheine <peter@taricorp.net>
2014-07-22 12:10:01 -0400

    Initial (untested) revision.

---
+++ src/ffi.rs
@@ -0,0 +1,206 @@
+#![allow(dead_code, missing_doc, uppercase_variables)]
+
+use libc::{c_void, c_int, c_long, size_t, c_float, c_char};
+
+// There's a lot of leakage from libvorbis into libvorbisfile.
+// TODO expose both libvorbis and libvorbisfile APIs.
+
+#[link(name="vorbisfile")]
+extern "C" {
+    pub fn ov_open_callbacks(datasource: *mut c_void, vf: *mut OggVorbis_File,
+                         initial: *mut c_char, ibytes: c_long,
+                         callbacks: ov_callbacks) -> c_int;
+
+    pub fn ov_read_float(vf: *mut OggVorbis_File, pcm_channels: *mut *mut *mut c_float,
+                         samples: c_int, bitstream: *mut c_int) -> c_long;
+
+    pub fn ov_info(vf: *mut OggVorbis_File, link: c_int) -> *const vorbis_info;
+}
+
+pub static OV_FALSE: c_int = -1;
+pub static OV_HOLE: c_int = -3;
+pub static OV_EREAD: c_int = -128;
+pub static OV_EFAULT: c_int = -129;
+pub static OV_EIMPL: c_int = -130;
+pub static OV_EINVAL: c_int = -131;
+pub static OV_ENOTVORBIS: c_int = -132;
+pub static OV_EBADHEADER: c_int = -133;
+pub static OV_EVERSION: c_int = -134;
+pub static OV_ENOTAUDIO: c_int = -135;
+pub static OV_EBADPACKET: c_int = -136;
+pub static OV_EBADLINK: c_int = -137;
+pub static OV_ENOSEEK: c_int = -138;
+
+#[repr(C)]
+pub struct OggVorbis_File {
+    pub datasource: *mut c_void,
+    offset: i64,
+    end: i64,
+    oy: ogg_sync_state,
+
+    links: int,
+    offsets: *mut i64,
+    dataoffsets: *mut i64,
+    serialnos: c_long,
+    pcmlengths: *mut i64,
+    vi: *mut vorbis_info,
+    vc: *mut vorbis_comment,
+
+    pcm_offset: i64,
+    ready_state: c_int,
+    current_serialno: c_long,
+    current_link: c_int,
+
+    bittrack: i64,
+    samptrack: i64,
+
+    os: ogg_stream_state,
+    vd: vorbis_dsp_state,
+    vb: vorbis_block,
+
+    callbacks: ov_callbacks,
+}
+
+#[repr(C)]
+pub struct ov_callbacks {
+    pub read: extern "C" fn(ptr: *mut c_void, size: size_t, nmemb: size_t, datasource: *mut c_void) -> size_t,
+    pub seek: extern "C" fn(datasource: *mut c_void, offset: i64, whence: c_int) -> c_int,
+    pub close: extern "C" fn(datasource: *mut c_void) -> c_int,
+    pub tell: extern "C" fn(datasource: *mut c_void) -> c_long,
+}
+
+#[repr(C)]
+pub struct vorbis_block {
+    pcm: *mut *mut c_float,
+    opb: oggpack_buffer,
+
+    lW: c_long,
+    W: c_long,
+    nW: c_long,
+    pcmend: c_int,
+    mode: c_int,
+
+    eofflag: c_int,
+    granulepos: i64,
+    sequence: i64,
+    vd: *const vorbis_dsp_state,
+
+    localstore: *mut c_void,
+    localtop: c_long,
+    localalloc: c_long,
+    totaluse: c_long,
+    read: *mut alloc_chain,
+
+    glue_bits: c_long,
+    time_bits: c_long,
+    floor_bits: c_long,
+    res_bits: c_long,
+
+    internal: *mut c_void,
+}
+
+#[repr(C)]
+pub struct alloc_chain {
+    ptr: *mut c_void,
+    next: *mut alloc_chain,
+}
+
+#[repr(C)]
+pub struct vorbis_comment {
+    user_comments: *mut *mut c_char,
+    comment_lengths: *mut c_int,
+    comments: c_int,
+    vendor: *mut c_char,
+}
+
+#[repr(C)]
+pub struct ogg_sync_state {
+    data: *mut c_char,
+    storage: c_int,
+    fill: c_int,
+    returned: c_int,
+
+    unsynced: c_int,
+    headerbytes: c_int,
+    bodybytes: c_int,
+}
+
+#[repr(C)]
+pub struct ogg_stream_state {
+    body_data: *mut c_char,
+    body_storage: c_long,
+    body_fill: c_long,
+    body_returned: c_long,
+
+    lacing_vals: *mut c_int,
+    granule_vals: *mut i64,
+    
+    lacing_storage: c_long,
+    lacing_fill: c_long,
+    lacing_packet: c_long,
+    lacing_returned: c_long,
+
+    header: [c_char, ..282],
+    header_fill: c_int,
+
+    e_o_s: c_int,
+    b_o_s: c_int,
+
+    serialno: c_long,
+    pageno: c_long,
+    packetno: i64,
+    granulepos: i64,
+}
+
+#[repr(C)]
+pub struct oggpack_buffer {
+    endbyte: c_long,
+    endbit: c_int,
+
+    buffer: *mut c_char,
+    ptr: *mut c_char,
+    storage: c_long,
+}
+
+#[repr(C)]
+pub struct vorbis_info {
+    pub version: c_int,
+    pub channels: c_int,
+    pub rate: c_long,
+
+    pub bitrate_upper: c_long,
+    pub bitrate_nominal: c_long,
+    pub bitrate_lower: c_long,
+    pub bitrate_window: c_long,
+
+    codec_setup: *mut c_void,
+}
+
+#[repr(C)]
+pub struct vorbis_dsp_state {
+    analysisp: c_int,
+    vi: *mut vorbis_info,
+
+    pcm: *mut *mut c_float,
+    pcmret: *mut *mut c_float,
+    pcm_storage: c_int,
+    pcm_currenet: c_int,
+    pcm_returned: c_int,
+
+    preextrapolate: c_int,
+    eofflag: c_int,
+
+    lW: c_long,
+    W: c_long,
+    nW: c_long,
+    centerW: c_long,
+
+    granulepos: i64,
+    sequence: i64,
+
+    glue_bits: i64,
+    time_bits: i64,
+    floor_bits: i64,
+    res_bits: i64,
+}
+
---
+++ src/lib.rs
@@ -0,0 +1,218 @@
+#![crate_name = "libvorbisfile"]
+#![doc(html_root_url = "http://www.rust-ci.org/tari/rust-ao/doc/ao/")]
+
+#![deny(dead_code, missing_doc)]
+
+//! Ogg Vorbis file decoding, library bindings.
+
+extern crate libc;
+use libc::{c_void, c_int, c_long, size_t};
+
+use std::mem;
+use std::ptr;
+use std::raw;
+use std::slice::raw::mut_buf_as_slice;
+
+mod ffi;
+
+pub type OVResult<T> = Result<T, OVError>;
+
+/// Decode error.
+#[deriving(Show, Clone)]
+pub enum OVError {
+    /// Reached end of file.
+    EndOfStream,
+    /// Encounted missing or corrupt data.
+    ///
+    /// Recovery from this error is usually automatic and is returned for
+    /// informational purposes only.
+    StreamInterrupted,
+    /// I/O error while reading compressed data to decode.
+    ReadError,
+    /// Internal inconsistency in encode or decode state. Recovery impossible.
+    InternalFault,
+    /// Feature not implemented.
+    NotImplemented,
+    /// User passed an invalid argument to a function.
+    InvalidArgument,
+    /// Provided data is not recognized as Ogg Vorbis.
+    NotVorbis,
+    /// Provided data appears to be Ogg Vorbis but has a corrupt or
+    /// indecipherable header.
+    InvalidHeader,
+    /// Bitstream format revision is not supported.
+    UnsupportedVersion,
+    /// The specified Vorbis link exists but is corrupt.
+    CorruptLink,
+    /// The stream is not seekable.
+    NotSeekable,
+}
+
+impl OVError {
+    fn from_native(code: c_int) -> OVError {
+        match code {
+            ffi::OV_HOLE => StreamInterrupted,
+            ffi::OV_EREAD => ReadError,
+            ffi::OV_EFAULT => InternalFault,
+            ffi::OV_EIMPL => NotImplemented,
+            ffi::OV_EINVAL => InvalidArgument,
+            ffi::OV_ENOTVORBIS => NotVorbis,
+            ffi::OV_EBADHEADER => InvalidHeader,
+            ffi::OV_EVERSION => UnsupportedVersion,
+            ffi::OV_EBADLINK => CorruptLink,
+            ffi::OV_ENOSEEK => NotSeekable,
+            x => fail!("Unexpected OVError code: {}", x)
+        }
+    }
+}
+
+/// Ogg Vorbis file decoder.
+pub struct VorbisFile<'a> {
+    // The FFI member holds a pointer to this box.
+    // Use a trait object because generic foreign functions are not allowed.
+    src: Box<Reader>,
+    decoder: ffi::OggVorbis_File,
+    // Totally not 'static, but need a lifetime specifier to get a slice.
+    channels: Vec<&'a [f32]>,
+}
+
+/// Read `nmemb` items into `ptr` of `size` bytes each.
+/// 
+/// If 0 is returned, error status is implied by errno. If nonzero, there was
+/// a read error. Otherwise, reached EOF.
+extern "C" fn read(ptr: *mut c_void, size: size_t, nmemb: size_t, datasource: *mut c_void) -> size_t {
+    let src = unsafe {
+        &mut *(datasource as *mut Box<Reader>)
+    };
+    let ptr = ptr as *mut u8;
+
+    for i in range(0, nmemb) {
+        let more = unsafe {
+            mut_buf_as_slice(ptr.offset(i as int), size as uint, |buf| {
+                match src.read_at_least(size as uint, buf) {
+                    Ok(_) => true,
+                    // Assume errno is set under the covers.
+                    Err(_) => false
+                }
+            })
+        };
+        if !more {
+            // Got error, which might be EOF.
+            return i;
+        }
+    }
+    // Completed successfully
+    return nmemb;
+}
+
+#[allow(unused_variable)]
+extern "C" fn seek(datasource: *mut c_void, offset: i64, whence: c_int) -> c_int {
+    // TODO permit seeking
+    -1
+}
+#[allow(unused_variable)]
+extern "C" fn close(datasource: *mut c_void) -> c_int {
+    // No need to do anything. VorbisFile owns the Reader we're using.
+    0
+}
+#[allow(unused_variable)]
+extern "C" fn tell(datasource: *mut c_void) -> c_long {
+    // TODO permit seeking
+    -1
+}
+
+// Lifetime bound on `src` is necessary because ffi::OggVorbis_File holds
+// a reference to it, so it must never move as long as this is alive.
+//
+// Don't expose ov_fopen and friends because that won't play nicely
+// with non-libnative runtime.
+impl<'a> VorbisFile<'a> {
+    /// Ensures the FFI struct is consistent for callback invocation.
+    ///
+    /// Because the user may move this struct, the `datasource` pointer
+    /// passed back to FFI callbacks might be invalidated. This function
+    /// should be called before FFI actions that might fire callbacks to ensure
+    /// the self-pointer is valid.
+    fn callback_setup(&mut self) {
+        let ds =  &mut self.src as *mut Box<Reader> as *mut c_void;
+        debug_assert!(ds == self.decoder.datasource);
+        self.decoder.datasource = ds;
+    }
+
+    /// Create a Ogg Vorbis decoder.
+    pub fn new<R: Reader+'static>(src: R) -> OVResult<VorbisFile<'a>> {
+        let mut vf = VorbisFile {
+            src: box src as Box<Reader>,
+            decoder: unsafe {
+                mem::uninitialized()
+            },
+            channels: Vec::new()
+        };
+        let callbacks = ffi::ov_callbacks {
+            read: read,
+            seek: seek,
+            tell: tell,
+            close: close,
+        };
+
+        let status = unsafe {
+            ffi::ov_open_callbacks(&mut vf.src as *mut Box<Reader> as *mut c_void, 
+                                   &mut vf.decoder, ptr::mut_null(), 0, callbacks)
+        };
+
+        match status {
+            0 => Ok(vf),
+            f => {
+                // Must not run the destructor. decoder is still uninitialized.
+                unsafe {
+                    mem::forget(vf.decoder);
+                }
+                Err(OVError::from_native(f))
+            }
+        }
+    }
+
+    /// Decode a block of samples.
+    ///
+    /// The emitted values are a slice of channels, each containing an equal
+    /// number of samples.
+    pub fn decode(&'a mut self) -> OVResult<&'a [&'a [f32]]> {
+        let max_samples = 4096;
+        self.callback_setup();
+        let mut sample_buffer: *mut *mut f32 = unsafe {
+            mem::uninitialized()
+        };
+        let mut bitstream_idx: c_int = unsafe {
+            mem::uninitialized()
+        };
+        
+        let n_samples = unsafe {
+            match ffi::ov_read_float(&mut self.decoder, &mut sample_buffer,
+                                     max_samples, &mut bitstream_idx) {
+                0 => {
+                    return Err(EndOfStream);
+                }
+                x if x < 0 => {
+                    return Err(OVError::from_native(x as c_int));
+                }
+                x => x
+            }
+        };
+        let n_channels = unsafe {
+            (*ffi::ov_info(&mut self.decoder, bitstream_idx)).channels
+        };
+
+        self.channels.truncate(0);
+        for i in range(0, n_channels) {
+            unsafe {
+                let channel_buffer = *sample_buffer.offset(i as int);
+                let channel_slice = raw::Slice::<f32> {
+                    data: channel_buffer as *const f32,
+                    len: n_samples as uint
+                };
+                self.channels.push(mem::transmute(channel_slice));
+            };
+        }
+        Ok(self.channels.as_slice())
+    }
+}
